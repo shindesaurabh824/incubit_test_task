009df90140acf6c2282e6e21d55f38dbf9ad5b87
Saurabh Shinde <shindesaurabh824@gmail.com>
2020-06-08 19:32:10 +0530

    Added navigation bar menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_user
+  helper_method :current_user, :user_logged_in?
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def user_logged_in?
+    current_user.present?
   end
 
   def sign_in(user)
